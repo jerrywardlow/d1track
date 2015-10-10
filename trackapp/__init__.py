709968cde730ab83f9a0a307ccb10f995126e6a9
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-10-10 13:30:55 -0700

    Added SQLAlchemy

--- trackapp/__init__.py
+++ trackapp/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
+
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+db = SQLAlchemy(app)
 
 import trackapp.views
 import trackapp.models
