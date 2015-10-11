d1e96a1c4818635a4633d17ae587e6bebbcd6840
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-10-11 12:44:59 -0700

    Added Address model

--- trackapp/models.py
+++ trackapp/models.py
@@ -17,3 +17,15 @@ class User(db.Model):
     active = db.Column(db.Boolean, default=True)
     created_on = db.Column(db.DateTime, default=datetime.now)
     # Need to add homegym_id relationship to Location
+
+class Address(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    address1 = db.Column(db.String)
+    address2 = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    zipcode = db.Column(db.Integer)
+    phone = db.Column(db.String) # Can expand to a phone table for multiples
+    email = db.Column(db.String) # Can expand to email table for multiples
+    website = db.Column(db.String)
+    location_id = db.Column(db.Integer, ForeignKey('locations.id'))
