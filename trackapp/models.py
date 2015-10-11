b84c75ce39e4484652cbfce0b2d6443eb22d9b22
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-10-11 15:54:51 -0700

    Updated errors with Location foreign relationships

--- trackapp/models.py
+++ trackapp/models.py
@@ -22,11 +22,11 @@ class User(db.Model):
     wingspan = db.Column(db.Float(precision=2)) # Centimeters
     shoesize = db.Column(db.Float(precision=1)) # European sizing
     # homegym_id = Column(Integer, ForeignKey('locations.id'))
-    # homegym = relationship(Location)
+    # homegym = db.relationship(Location)
     photo = db.Column(db.String)
     active = db.Column(db.Boolean, default=True)
     created_on = db.Column(db.DateTime, default=datetime.now)
-    # Need to update homegym_id relationship to Location
+    # Need to update homegym_id db.relationship to Location
 
     def __repr__(self):
         return '<User %r>' % (self.username)
@@ -42,6 +42,7 @@ class Address(db.Model):
     email = db.Column(db.String) # Can expand to email table for multiples
     website = db.Column(db.String)
     location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    location = db.relationship(Location)
 
     def __repr__(self):
         return '<Address ID: %r>' % (self.id)
@@ -51,6 +52,7 @@ class Zone(db.Model):
     name = db.Column(db.String)
     photo = db.Column(db.String)
     location_id = db.Column(db.Integer, ForeignKey('locations.id'))
+    location = db.relationship(Location)
 
     def __repr__(self):
         return '<Zone %r>' % (self.name)
@@ -59,7 +61,7 @@ class Anchor(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
     zone_id = db.Column(db.Integer, db.ForeignKey('zones.id'))
-    zone = relationship(Zone)
+    zone = db.relationship(Zone)
     # style_id = db.Column(db.Integer)
     # draw_id = db.Column(db.Integer)
     # rope_id = db.Column(db.Integer)
@@ -68,9 +70,9 @@ class Route(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
     anchor_id = db.Column(db.Integer, db.ForeignKey('anchors.id'))
-    anchor = relationship(Anchor)
+    anchor = db.relationship(Anchor)
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
-    location = relationship(Location)
+    location = db.relationship(Location)
     defined_grade = db.Column(db.Integer)
     length = db.Column(db.Integer)
     # setter_id = db.Column(db.Integer)
