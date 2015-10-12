a9b1b68fcd2031995d2f36be0499d4b856c9d00e
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-10-12 16:57:46 -0700

    Added comments for clarity

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@ apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-pip
 
+# PIP installs
 pip install flask
 pip install Flask-SQLAlchemy
 pip install Flask-WTF
@@ -10,17 +11,22 @@ pip install requests
 pip install httplib2
 pip install pyimgur
 
+# Create Postgres user Vagrant and create 'trackdb' database
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb trackdb'
 
+# Run `db_populator.py` to initialize tables and populate with sample data
 su vagrant -c 'python /vagrant/db_populator.py'
 
-echo 'cd /vagrant' > /home/vagrant/.bash_profile # This line tells where Vagrant should head to after 'vagrant ssh'
+# Add to '.bash_profile' to change to shared directory on SSH
+echo 'cd /vagrant' > /home/vagrant/.bash_profile
 
+# Add string to the MOTD
 MOTDblast="[36m[1mThis is a placeholder for the MOTD[m"
 echo -e $MOTDblast > /etc/motd
 
+# Symlink .bask_profile if necessary
 if [ ! -f /home/vagrant/.bash_profile ]; then
   ln -s /vagrant/.bash_profile /home/vagrant/.bash_profile
 fi
