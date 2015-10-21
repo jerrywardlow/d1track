8ff4b4ddc2f5e414124b003c1905634436bee27a
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-10-21 12:09:47 -0700

    Added `location_home` for testing templates

--- trackapp/views.py
+++ trackapp/views.py
@@ -1,7 +1,16 @@
 from flask import render_template
 
-from trackapp import app
+from trackapp import app, db
+
+from .models import Location, User, Address, Zone, Anchor, Route
 
 @app.route('/')
+@app.route('/index/')
 def index():
     return "Everything seems fine..."
+
+@app.route('/location/<int:location_id>/')
+def location_home(location_id):
+    location = Location.query.get(location_id)
+    return render_template('location_home.html',
+                            location = location)
