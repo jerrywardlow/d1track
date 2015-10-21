7250fa1aef6d2db49c835492e41abf84bd397aba
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-10-21 14:28:05 -0700

    Added test User

--- trackapp/views.py
+++ trackapp/views.py
@@ -11,8 +11,10 @@ def index():
 
 @app.route('/location/<int:location_id>/')
 def location_home(location_id):
+    user = User.query.get(1)
     location = Location.query.get(location_id)
     address = Address.query.filter_by(location_id=location_id).first()
     return render_template('location_home.html',
+                            user = user,
                             location = location,
                             address = address)
