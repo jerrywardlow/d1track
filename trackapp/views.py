713f516d1908f0e2f4b1e43217a745ca41189bf0
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-10-22 20:59:53 -0700

    Import of all models

--- trackapp/views.py
+++ trackapp/views.py
@@ -2,7 +2,7 @@ from flask import render_template
 
 from trackapp import app, db
 
-from .models import Location, User, Address, Zone, Anchor, Route
+from .models import Location, User, Address, Zone, Anchor, Route, Sesh, Climb
 
 @app.route('/')
 @app.route('/index/')
