d0766288c214bef8ac219d610d397b05bee65bd1
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-04 23:16:20 -0800

    Suppressing SQLAlchemy warnings

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ SECRET_KEY = 'secret_key'
 WTF_CSRF_ENABLED = True
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://trackingdbuser:superawesomepassword@localhost/trackdb'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
