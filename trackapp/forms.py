e1571ab505e19b0d05c35cffaa4106f0b91cd25a
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-05 21:36:32 -0800

    Basic framework for testing

--- trackapp/forms.py
+++ trackapp/forms.py
@@ -1,1 +1,7 @@
 from flask.ext.wtf import Form
+from wtforms import BooleanField, StringField, validators
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=30)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    accept_rules = BooleanField('I accept the site rules', [validators.InputRequired()])
