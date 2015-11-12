e9fb349f8b0027f79199909c5c3e4c1bdacbb624
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-11 23:36:28 -0800

    Formatting and imports

--- trackapp/forms.py
+++ trackapp/forms.py
@@ -1,7 +1,8 @@
 from flask.ext.wtf import Form
-from wtforms import BooleanField, StringField, validators
+from wtforms import BooleanField, StringField
+from wtforms.validators import Length, InputRequired
 
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=30)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    accept_rules = BooleanField('I accept the site rules', [validators.InputRequired()])
+    username = StringField('Username', validators=[Length(min=4, max=30)])
+    email = StringField('Email Address', validators=[Length(min=6, max=35)])
+    accept_rules = BooleanField('I accept the site rules', validators=[InputRequired()])
