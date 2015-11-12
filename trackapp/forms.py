222cdfa8b16e2efea619860d4ee86f43ca9841bd
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-11 23:47:01 -0800

    Changed validators for RegistrationForm

--- trackapp/forms.py
+++ trackapp/forms.py
@@ -1,8 +1,8 @@
 from flask.ext.wtf import Form
 from wtforms import BooleanField, StringField
-from wtforms.validators import Length, InputRequired
+from wtforms.validators import Length, InputRequired, Email
 
 class RegistrationForm(Form):
-    username = StringField('Username', validators=[Length(min=4, max=30)])
-    email = StringField('Email Address', validators=[Length(min=6, max=35)])
+    username = StringField('Username', validators=[InputRequired('Please choose a username'), Length(min=4, max=30)])
+    email = StringField('Email Address', validators=[InputRequired('Please enter a valid e-mail address'), Email('Please enter a valid e-mail address')])
     accept_rules = BooleanField('I accept the site rules', validators=[InputRequired()])
