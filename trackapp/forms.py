2a21998f6610e013e8983b2ea53912a57908249b
Jerry Wardlow <jerrylikesbikes@gmail.com>
2015-11-15 15:02:28 -0800

    Added 'submit' to RegistrationForm

--- trackapp/forms.py
+++ trackapp/forms.py
@@ -1,11 +1,12 @@
 from flask.ext.wtf import Form
-from wtforms import BooleanField, StringField, FloatField, TextAreaField
+from wtforms import BooleanField, StringField, FloatField, TextAreaField, SubmitField
 from wtforms.validators import Length, InputRequired, Email
 
 class RegistrationForm(Form):
     username = StringField('Username', validators=[InputRequired('Please choose a username'), Length(min=4, max=30)])
     email = StringField('Email Address', validators=[InputRequired('Please enter a valid e-mail address'), Email('Please enter a valid e-mail address')])
     accept_rules = BooleanField('I accept the site rules', validators=[InputRequired()])
+    submit = SubmitField('Submit')
 
 class UserProfileForm(Form):
     username = StringField()
