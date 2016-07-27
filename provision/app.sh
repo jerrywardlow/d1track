54f45bc7c9ea8aab29f95e2f6463050e2a610a72
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-27 16:04:03 -0700

    Removed MOTD

--- provision/app.sh
+++ provision/app.sh
@@ -24,7 +24,3 @@ su vagrant -c 'python /var/www/d1track/db_populator.py'
 
 # Add to '.bash_profile' to change to shared directory on SSH
 echo 'cd '$1 > /home/vagrant/.bash_profile
-
-# Add string to the MOTD
-MOTDblast="[36m[1mThis is a placeholder for the MOTD[m"
-echo -e $MOTDblast > /etc/motd
