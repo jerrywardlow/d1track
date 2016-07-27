c99aae7cddd20a7cbcbf4904255bfafd58c6ef6b
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-27 16:34:10 -0700

    Broke out URI into distinct units

--- config.py
+++ config.py
@@ -3,5 +3,14 @@ SECRET_KEY = 'secret_key'
 
 WTF_CSRF_ENABLED = True
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://pgdbuser:pgpassword@localhost/trackdb'
+db_driver = 'postgresql'
+db_user = 'pgdbuser'
+db_password = 'pgpassword'
+db_host = 'localhost'
+db_port = '5432'
+db_name = 'trackdb'
+
+SQLALCHEMY_DATABASE_URI = '{0}://{1}:{2}@{3}:{4}/{5}'.format(
+    db_driver, db_user, db_password, db_host, db_port, db_name
+)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
