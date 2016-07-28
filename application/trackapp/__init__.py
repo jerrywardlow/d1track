fd5cdb273b9327b4f2911c2ac2633175af17810c
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-27 17:17:20 -0700

    Changed configuration style

--- application/trackapp/__init__.py
+++ application/trackapp/__init__.py
@@ -3,10 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_superadmin import Admin
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 
 # Basic Flask configuration
-app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 # Flask-SQLAlchemy configuration
