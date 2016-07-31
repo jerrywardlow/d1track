6d7e3671b8a07c0bc609a60249f64bd9697694e3
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-07-31 12:05:24 -0700

    Open Postgres to app server

--- provision/db.sh
+++ provision/db.sh
@@ -3,3 +3,6 @@ apt-get -qqy install postgresql
 
 su postgres -c "psql -c \"CREATE USER pgdbuser with password 'pgpassword';\""
 su postgres -c 'createdb trackdb'
+
+echo 'host all all 172.22.22.201' >> /etc/postgresql/$(ls /etc/postgresql)/main/pg_hba.conf
+echo "listen_addresses='172.22.22.201'" >> /etc/postgresql/$(ls /etc/postgresql)/main/postgresql.conf
