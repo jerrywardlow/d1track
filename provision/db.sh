dc4498e6d0e39653fbc3ea07de9c0ce0b97f30c0
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-11 20:33:31 -0700

    Accept connections from entire /24 address block

--- provision/db.sh
+++ provision/db.sh
@@ -4,7 +4,7 @@ apt-get -qqy install postgresql
 su postgres -c "psql -c \"CREATE USER pgdbuser with password 'pgpassword';\""
 su postgres -c 'createdb trackdb'
 
-echo 'host all all 172.22.22.201/32 password' >> /etc/postgresql/$(ls /etc/postgresql)/main/pg_hba.conf
+echo 'host all all 192.168.56.0/24 password' >> /etc/postgresql/$(ls /etc/postgresql)/main/pg_hba.conf
 echo "listen_addresses='*'" >> /etc/postgresql/$(ls /etc/postgresql)/main/postgresql.conf
 
 service postgresql restart
