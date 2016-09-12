d3361a6badc5b02af9c1190e09e5eaa9e632bcae
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-09-11 20:29:58 -0700

    Listen on all available addresses

--- provision/db.sh
+++ provision/db.sh
@@ -5,6 +5,6 @@ su postgres -c "psql -c \"CREATE USER pgdbuser with password 'pgpassword';\""
 su postgres -c 'createdb trackdb'
 
 echo 'host all all 172.22.22.201/32 password' >> /etc/postgresql/$(ls /etc/postgresql)/main/pg_hba.conf
-echo "listen_addresses='172.22.22.202'" >> /etc/postgresql/$(ls /etc/postgresql)/main/postgresql.conf
+echo "listen_addresses='*'" >> /etc/postgresql/$(ls /etc/postgresql)/main/postgresql.conf
 
 service postgresql restart
